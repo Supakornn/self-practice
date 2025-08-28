334946d5b4167c4198cb9e5e0563f07a51e7e3ad
Supakornn <78318848+Supakornn@users.noreply.github.com>
2025-08-28 18:30:00 +0700

    week04: add leetcode practice 1.

---
+++ week04/index.js
@@ -0,0 +1,15 @@
+// 1. Median of Two Sorted Arrays
+var findMedianSortedArrays = function (nums1, nums2) {
+    const arr = [...nums1, ...nums2].sort((a, b) => a - b);
+    const mid = Math.floor(arr.length / 2);
+    if (arr.length % 2 === 0) {
+        return (arr[mid - 1] + arr[mid]) / 2;
+    } else {
+        return arr[mid];
+    }
+};
+
+console.log(findMedianSortedArrays([1, 3], [2]));
+console.log(findMedianSortedArrays([1, 2], [3, 4]));
+
+// -----------------------------------------------------
